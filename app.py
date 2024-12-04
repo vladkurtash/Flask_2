baa9ad73b60301b98a0f6a28d12b0d7c739fc48f
vladkurtash <er.vladkurtash@gmail.com>
2024-12-05 01:57:45 +0200

    Add Main Files

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, Blueprint
+
+def create_app(test_config=None):
+    app = Flask(__name__, 
+        template_folder='flaskr/templates',
+        static_folder='flaskr/static',
+        static_url_path=''
+    )
+    
+    # Create a main blueprint for our basic pages
+    main = Blueprint('main', __name__)
+    
+    @main.route('/')
+    def index():
+        return render_template('blog/index.html')
+
+    @main.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @main.route('/contacts')
+    def contacts():
+        return render_template('contacts.html')
+
+    @main.route('/services')
+    def services():
+        return render_template('services.html')
+
+    # Register the main blueprint
+    app.register_blueprint(main)
+
+    # Register other blueprints
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5000)
---
+++ flaskr/db.py
@@ -0,0 +1,43 @@
+import sqlite3
+import click
+from flask import current_app, g
+from datetime import datetime
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Очистити існуючі дані і створити нові таблиці."""
+    init_db()
+    click.echo('База даних ініціалізована.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+sqlite3.register_converter('timestamp', lambda x: datetime.fromisoformat(x.decode())) 
